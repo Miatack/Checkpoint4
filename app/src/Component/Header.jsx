3dbafa665386a9a830edbe4c63d19cbbac0827d8
Miatack <miatack529@gmail.com>
2019-07-19 10:22:30 +0200

    Navbar with disabled link

--- app/src/Component/Header.jsx
+++ app/src/Component/Header.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import './Header.scss'
+import Navbar from './Header/Navbar.jsx'
 
 export default class Header extends Component {
   render() {
     return (
       <div>
         <h1>WILD CIRCUS</h1>
+        <Navbar />
       </div>
     )
   }
---
+++ app/src/Component/Header/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Nav, NavItem, NavLink } from 'reactstrap';
+import './Navbar.scss'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <Nav>
+          <NavItem>
+            <NavLink disabled href="#">Performance</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink disabled href="#">Program</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink disabled href="#">Map</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink disabled href="#">Shop</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink disabled href="#">About</NavLink>
+          </NavItem>
+        </Nav>
+      </div>
+    )
+  }
+}
